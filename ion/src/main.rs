dcb1a89feb6fb8db00e399dda1248bd25364c748
Justin Liew <justinliew@gmail.com>
2019-03-07 13:59:22 -0800

    working on it misses characters after a multi-char token match because of iterator consumption.

--- ion/src/main.rs
+++ ion/src/main.rs
@@ -5,36 +5,62 @@
 enum Token {
     Integer(u64),
     Name(String),
+    Symbol(char),
 }
 
 fn print_token(t: Token) {
     match t {
-        Token::Integer(i) => println!("{}",i),
-        Token::Name(n) => println!("{}",n),
+        Token::Integer(i) => println!("Int: {}",i),
+        Token::Name(n) => println!("Name: {}",n),
+        Token::Symbol(s) => println!("Symbol: {}", s),
     }
 }
 
 fn next_token(stream_iter : &mut std::str::Chars) -> Option<Token> {
 
-    let next = match stream_iter.next() {
+    let mut next = match stream_iter.next() {
         Some(x) => x,
         None => return None
     };
 
-    let t = match next {
+    // TODO: I'm taking the approach from a C implementation of this
+    // can we do this properly with just match, instead of the inner loop iterations?
+    // also this consumes the character after an integer or name token since we are advancing the iterator, failing to match and then returning
+    match next {
         '0'..='9' => {
-            Some(Token::Integer(next as u64))
+            let mut val : u64 = 0;
+            while next.is_digit(10) {
+                let digit = next as u64 - ('0' as u64);
+                val *= 10;
+                val += digit;                
+                next = match stream_iter.next() {
+                    Some(x) => x,
+                    None => {
+                        return Some(Token::Integer(val))
+                    }
+                };
+            }
+            return Some(Token::Integer(val))
         },
-        _ => {
-            None
-        }
+        'a'..='z' | 'A'..='Z' | '_' => {
+            let mut name = String::new();
+            while next.is_alphanumeric() {
+                name.push(next);
+                next = match stream_iter.next() {
+                    Some(x) => x,
+                    None => {
+                        return Some(Token::Name(name))
+                    }
+                };
+            }
+            return Some(Token::Name(name))
+        },
+        _ => return Some(Token::Symbol(next))
     };
-    t
 }
 
 fn lex_test() {
-//    let input = "+()1234+994";
-    let input = "1234567";
+    let input = "+()1234+994abc";
     let mut iter = input.chars();
     loop  {
         let t = next_token(&mut iter);
