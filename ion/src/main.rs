7b6aa5425c230cc0b90b5489fcf0e0b5a322e212
Justin Liew <justinliew@gmail.com>
2019-03-06 23:43:49 -0800

    lexing part 1

--- ion/src/main.rs
+++ ion/src/main.rs
@@ -4,18 +4,47 @@
 
 enum Token {
     Integer(u64),
+    Name(String),
 }
 
-fn next_token(s : &str) {
-    
+fn print_token(t: Token) {
+    match t {
+        Token::Integer(i) => println!("{}",i),
+        Token::Name(n) => println!("{}",n),
+    }
+}
+
+fn next_token(stream_iter : &mut std::str::Chars) -> Option<Token> {
+
+    let next = match stream_iter.next() {
+        Some(x) => x,
+        None => return None
+    };
+
+    let t = match next {
+        '0'..='9' => {
+            Some(Token::Integer(next as u64))
+        },
+        _ => {
+            None
+        }
+    };
+    t
 }
 
 fn lex_test() {
-    let input = "+()1234+994";
-    next_token(&input);
+//    let input = "+()1234+994";
+    let input = "1234567";
+    let mut iter = input.chars();
+    loop  {
+        let t = next_token(&mut iter);
+        match t {
+            Some(token) => print_token(token),
+            None => break
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
     lex_test()
 }
