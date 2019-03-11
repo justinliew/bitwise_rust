e3653b7af7b7d163d686c0dd1d38012615ca7352
Justin Liew <justinliew@gmail.com>
2019-03-11 14:50:36 -0700

    bytecode stack vm doesn't handle NEG, HALT or negative numbers, but I think it's a good enough proof of concept

--- ion/src/bytecode_stack.rs
+++ ion/src/bytecode_stack.rs
@@ -4,13 +4,13 @@ use std::vec;
 
 // we should be able to take an expression, convert it to bytecode, and then write the bytecode stack machine to run it and output the result.
 
-const LIT : u32 = 1;
-const ADD : u32 = 2;
-const SUB : u32 = 3;
-const MUL : u32 = 4;
-const DIV : u32 = 5;
-const NEG : u32 = 6;
-const HALT : u32 = 7;
+const LIT : u64 = 1;
+const ADD : u64 = 2;
+const SUB : u64 = 3;
+const MUL : u64 = 4;
+const DIV : u64 = 5;
+const NEG : u64 = 6;
+const HALT : u64 = 7;
 
 // expr3 = INT | '(' expr ')' 
 // expr2 = '-'? expr2 | q120
@@ -19,11 +19,11 @@ const HALT : u32 = 7;
 // expr = expr0
 // (1+2)*3
 
-fn parse_expr3(stream: &mut LexStream) -> Vec<u32> {
+fn parse_expr3(stream: &mut LexStream) -> Vec<u64> {
     match stream.get_token() {
         Some(Token::Integer(n)) => {
             stream.next_token();
-            vec![LIT,n as u32]
+            vec![LIT,n as u64]
         },
         Some(Token::Symbol(s)) => {
             if s == '(' {
@@ -46,7 +46,7 @@ fn parse_expr3(stream: &mut LexStream) -> Vec<u32> {
     }
 }
 
-fn parse_expr2(stream: &mut LexStream) -> Vec<u32> {
+fn parse_expr2(stream: &mut LexStream) -> Vec<u64> {
     match stream.get_token() {
         Some(n) => {
             if n.match_token(&Token::Symbol('-'), stream) {
@@ -61,7 +61,7 @@ fn parse_expr2(stream: &mut LexStream) -> Vec<u32> {
     }
 }
 
-fn parse_expr1(stream: &mut LexStream) -> Vec<u32> {
+fn parse_expr1(stream: &mut LexStream) -> Vec<u64> {
     let mut val = parse_expr2(stream);
     loop {
         match stream.get_token() {
@@ -88,7 +88,7 @@ fn parse_expr1(stream: &mut LexStream) -> Vec<u32> {
     val
 }
 
-fn parse_expr0(stream: &mut LexStream) -> Vec<u32> {
+fn parse_expr0(stream: &mut LexStream) -> Vec<u64> {
     let mut val = parse_expr1(stream);
     loop {
         match stream.get_token() {
@@ -117,30 +117,90 @@ fn parse_expr0(stream: &mut LexStream) -> Vec<u32> {
     val
 }
 
-fn parse_expr(stream: &mut LexStream) -> Vec<u32> {
+fn parse_expr(stream: &mut LexStream) -> Vec<u64> {
     stream.next_token();
     parse_expr0(stream)
 }
 
-fn run_bytecode(code: &[u32]) -> u64 {
-    42
+/*
+const LIT : u64 = 1;
+const ADD : u64 = 2;
+const SUB : u64 = 3;
+const MUL : u64 = 4;
+const DIV : u64 = 5;
+const NEG : u64 = 6;
+const HALT : u64 = 7;
+
+*/
+fn run_bytecode(code: &[u64]) -> u64 {
+    println!("Bytecode stream: {:?}", code);
+    let mut iter = code.iter();
+    let mut stack : Vec<u64> = vec![];
+
+    loop {
+        let v = match iter.next() {
+            Some(x) => x,
+            None => return *stack.first().unwrap() // TODO ensure this is the right answer and that the stack is 1 element
+        };
+
+        println!("Handling opcode: {}", v);
+        println!("Current stack: {:?}", stack);
+        match *v {
+            LIT => {
+                let literal = match iter.next() {
+                    Some(x) => x,
+                    None => {
+                        assert!(false, "malformed bytecode; missing a literal after a LIT op"); 
+                        return 0;
+                    }
+                };
+                stack.push(*literal);
+            },
+            ADD => {
+                assert!(stack.len()>=2, "malformed bytecode; not enough operands for ADD op");
+                let lval = stack.pop().unwrap();
+                let rval = stack.pop().unwrap();
+                stack.push(lval + rval);
+            },
+            SUB => {
+                assert!(stack.len()>=2, "malformed bytecode; not enough operands for SUB op");
+                let lval = stack.pop().unwrap();
+                let rval = stack.pop().unwrap();
+                stack.push(rval - lval);
+            },
+            MUL => {
+                assert!(stack.len()>=2, "malformed bytecode; not enough operands for MUL op");
+                let lval = stack.pop().unwrap();
+                let rval = stack.pop().unwrap();
+                stack.push(rval * lval);
+            },
+            DIV => {
+                assert!(stack.len()>=2, "malformed bytecode; not enough operands for DIV op");
+                let lval = stack.pop().unwrap();
+                let rval = stack.pop().unwrap();
+                assert!(rval == 0, "cannot divide by 0");
+                stack.push(rval / lval);
+            },
+            NEG => {
+                // TODO how do we handle negative numbers?
+                // assert!(stack.len()>=1, "malformed bytecode; not enough operands for SUB op");
+                // let val = stack.pop().unwrap();
+                // stack.push(-val);
+            },
+            HALT => {
+
+            },
+            _ => {}
+        }
+    }
 }
 
-// LIT x
-// ADD
-// SUB
-// MUL
-// DIV
-
-fn gen_bytecode(stream: &mut LexStream) -> Vec<u32> {
-    let res = parse_expr(stream);
-    println!("gen_bytecode: {:?}", res);
-    res
+fn gen_bytecode(stream: &mut LexStream) -> Vec<u64> {
+    parse_expr(stream)
 }
 
 pub fn bytecode_stack_test() {
-//    let input = "-3+4+(5*6)";
-    let input = "-1+2";
+    let input = "3+4+(5*6)";
     let mut stream = LexStream::init(input);
 
     let bytecode = gen_bytecode(&mut stream);
